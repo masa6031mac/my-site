f17090933d1e73902d3faf8ae6039836a52be21c
m_nakagawa <masa6031.mac@gmail.com>
2022-08-23 13:29:00 +0900

    test5

--- app.py
+++ app.py
@@ -13,16 +13,17 @@ def user_list():
         'Taro','jiro','Saburo','Shiro','Hanako','Masa'
     ]
     is_login = False
-    return render_template('userlist.html', users=users, is_login=is_login)
+    if host == my_host:
+        return render_template('index.html', users=users, is_login=is_login)
+    else:
+        return render_template('userlist.html', users=users, is_login=is_login)
 
 if __name__ == '__main__':
     host = socket.gethostname()
     my_host = "MasahironoMacBook-Pro.local"
 
     if host == my_host:
-        print('ローカル実行')
         app.run(debug=True)
     else:
-        print('本番実行')
         app.run()
 
