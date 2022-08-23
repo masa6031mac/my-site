fdbe9485a39189cb5eebdd07850f5ccaeec162e8
m_nakagawa <masa6031.mac@gmail.com>
2022-08-23 13:26:49 +0900

    test4

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+import socket
 
 app = Flask(__name__)
 
@@ -9,10 +10,19 @@ def index():
 @app.route('/userlist')
 def user_list():
     users = [
-        'Taro','jiro','Saburo','Shiro'
+        'Taro','jiro','Saburo','Shiro','Hanako','Masa'
     ]
     is_login = False
     return render_template('userlist.html', users=users, is_login=is_login)
 
 if __name__ == '__main__':
-    app.run()
+    host = socket.gethostname()
+    my_host = "MasahironoMacBook-Pro.local"
+
+    if host == my_host:
+        print('ローカル実行')
+        app.run(debug=True)
+    else:
+        print('本番実行')
+        app.run()
+
