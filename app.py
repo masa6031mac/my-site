99301b16cd4b22d780cb94f2a3eca5b7af989007
m_nakagawa <masa6031.mac@gmail.com>
2022-08-23 13:16:22 +0900

    test4

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/userlist')
+def user_list():
+    users = [
+        'Taro','jiro','Saburo','Shiro'
+    ]
+    return render_template('userlist.html', users=users)
+
 if __name__ == '__main__':
     app.run()
