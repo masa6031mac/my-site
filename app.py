6d6b22e19efac9b930513bb1b44fd3cdf6819dd4
m_nakagawa <masa6031.mac@gmail.com>
2022-08-23 12:49:16 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home/<string:user_name>/<int:age>')
+def home(user_name, age):
+    #login_user = user_name
+    login_user = {
+        'name': user_name,
+        'age': age
+    }
+    return render_template('home.html',login_user=login_user)
+
+if __name__ == '__main__':
+    app.run(debug=True)
